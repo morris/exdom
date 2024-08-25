ea30154ab690315bf748b0a8fc443ca9afd64f03
Morris Brodersen <mb@morrisbrodersen.de>
2024-08-25 17:39:10 +0200

    update deps

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: [
+      'coverage',
+      'dist',
+      'docs',
+      'node_modules',
+      'test-build',
+      'test-results',
+    ],
+  },
+);
